f1e4700df8c054b1846d91f0a1694942144cb482
Martin Kopecký <kopecky@thekrew.app>
2024-02-15 13:32:11 +0100

    Development (#5) (#6)
    * Bump framer-motion from 10.18.0 to 11.0.5
    
    Bumps [framer-motion](https://github.com/framer/motion) from 10.18.0 to 11.0.5.
    - [Changelog](https://github.com/framer/motion/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/framer/motion/compare/v10.18.0...v11.0.5)
    
    ---
    updated-dependencies:
    - dependency-name: framer-motion
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    
    
    * Create codeql.yml
    
    * Create eslint.yml
    
    * Delete .github/workflows/codeql.yml
    
    * Navbar rework and easy select for filaments
    
    * v0.4.0
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,41 +1,82 @@
-import { Button } from '@nextui-org/react'
+import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
+import {
+ Button,
+ Dropdown,
+ DropdownTrigger,
+ DropdownMenu,
+ DropdownItem,
+ Divider,
+} from '@nextui-org/react'
+import { RefObject } from 'react'
 
 function Navbar(props: {
- ShowAddScreen: () => void
  ShowTable: () => void
  Calculate: () => void
  defaultLoad: boolean
+ AddRef: RefObject<HTMLDivElement>
+ EditRef: RefObject<HTMLDivElement>
+ RemoveRef: RefObject<HTMLDivElement>
 }) {
- return (
-  <nav className="grid grid-cols-2 py-2 bg-gray-800 min-h-10 mb-5 px-3">
-   <div className="flex gap-5 ">
-    <Button
-     type="button"
-     color="warning"
-     onClick={() => props.ShowAddScreen()}
-     className="w-max"
-     isDisabled={props.defaultLoad}>
-     Add
-    </Button>
+ function Show(ref: RefObject<HTMLDivElement>) {
+  const main = ref.current || null
+  if (main) {
+   if (main?.classList.contains('off')) {
+    main?.classList.remove('off')
+   } else {
+    main?.classList.add('off')
+   }
+  }
+ }
 
+ return (
+  <nav className="grid grid-cols-2 py-2 bg-slate-900 min-h-10 mb-5 px-3">
+   <div className="flex gap-5 ">
+    <Dropdown className="text-white">
+     <DropdownTrigger>
+      <Button variant="light" color="primary" className="font-bold text-lg">
+       Filaments
+      </Button>
+     </DropdownTrigger>
+     <DropdownMenu aria-label="Static Actions" className="text-black">
+      <DropdownItem
+       key="new"
+       startContent={<PlusIcon className="w-4 h-4" />}
+       variant="solid"
+       description="Add filament to configuration file"
+       onClick={() => Show(props.AddRef)}>
+       New filament
+      </DropdownItem>
+      <DropdownItem
+       key="edit"
+       startContent={<PencilIcon className="w-4 h-4" />}
+       description="Edit filaments configuration"
+       variant="solid"
+       onClick={() => Show(props.EditRef)}>
+       Edit filament
+      </DropdownItem>
+      <DropdownItem
+       key="edit"
+       startContent={<TrashIcon className="w-4 h-4" />}
+       description="Remove filament from configuration file"
+       color="danger"
+       variant="solid"
+       onClick={() => Show(props.RemoveRef)}>
+       Remove filament
+      </DropdownItem>
+     </DropdownMenu>
+    </Dropdown>
+    <Divider orientation="vertical" className="bg-gray-500" />
     <Button
+     variant="light"
      type="button"
      color="success"
      onClick={() => props.ShowTable()}
-     className="w-max"
+     className="w-max font-bold text-lg"
      isDisabled={props.defaultLoad}>
      Select filament
     </Button>
    </div>
-   <div className="flex justify-end">
-    <Button
-     type="button"
-     color="primary"
-     className="w-max right-0"
-     onClick={() => props.Calculate()}>
-     Calculate
-    </Button>
-   </div>
+   <div className="flex justify-end"></div>
   </nav>
  )
 }
